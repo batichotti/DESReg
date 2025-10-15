568c33bfba597c0d638e450147c7021d9259778e
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2025-10-15 20:44:06 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,13 +1,20 @@
 import numpy as np
 import change_parameters
 
-mse_list = []
-for i in range(100):
-    mse_list.append(change_parameters.test())
 
-print(mse_list)
-mse_numpy = np.array(mse_list)
-print("Média:", np.mean(mse_numpy))
-print("Mediana:", np.median(mse_numpy))
-print("Desvio padrão:", np.std(mse_numpy))
-print("Coeficiente de Variação:", (np.std(mse_numpy)/np.mean(mse_list))*100) # CV baixo (< 15%~20%) indica dispersão relativamente baixa
+def simulate(amount: int = 100) -> list[float]:
+    mse_list = list[float]
+    for i in range(100):
+        mse_list.append(change_parameters.test())
+    return mse_list
+
+    
+if __name__ == "__main__":
+    mse_list = simulate(30)
+
+    print(mse_list)
+    mse_numpy = np.array(mse_list)
+    print("Média:", np.mean(mse_numpy))
+    print("Mediana:", np.median(mse_numpy))
+    print("Desvio padrão:", np.std(mse_numpy))
+    print("Coeficiente de Variação:", (np.std(mse_numpy)/np.mean(mse_list))*100) # CV baixo (< 15%~20%) indica dispersão relativamente baixa
