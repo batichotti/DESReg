70048b96267c8965df2a87c85659b5b4e001269a
Matt Batichotti <matebatichotti@gmail.com>
2025-11-17 14:31:21 -0300

    Rename: change_parameters -> features

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn.model_selection import RepeatedKFold
-from change_parameters import load_dataset, test
+from features import load_dataset, test
 from scipy.spatial import distance
 from dataclasses import dataclass
 import pandas as pd
@@ -11,7 +11,7 @@ from tqdm import tqdm
 COMPETENCE_REGION_LIST = ['knn', 'cluster', 'output_profiles']
 DISTANCE_HEURISTICS_LIST = [distance.braycurtis, distance.canberra, distance.chebyshev, distance.cityblock, distance.cosine, distance.euclidean, distance.minkowski, distance.sqeuclidean]
 # DATASETS_LIST = ['student_marks', 'liver', 'yatch', 'housing', 'real_estate', 'concrete', 'triazines', 'stock', 'airfoild', 'wine_quality_red', 'abalone', 'wine_quality_white', 'ccpp', 'delta_elevators', 'bank8fm', 'puma8nh', 'puma32h', 'bank32nh']
-DATASETS_LIST = ['student_marks', 'liver', 'yatch', 'housing', 'real_estate', 'concrete', 'triazines', 'stock', 'airfoild', 'wine_quality_red', 'abalone', 'wine_quality_white', 'ccpp', 'delta_elevators', 'bank8fm', 'puma8nh', 'puma32h', 'bank32nh']
+DATASETS_LIST = ['housing', 'real_estate', 'concrete', 'triazines', 'stock', 'airfoild', 'wine_quality_red', 'abalone', 'wine_quality_white', 'ccpp', 'delta_elevators', 'bank8fm', 'puma8nh', 'puma32h', 'bank32nh']
 
 def make_distance_safe(dist_func):
     def safe_dist(u, v):
